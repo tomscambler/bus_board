5dd4b94a03b85ca549bf767499c1b398d08063a3
tomscambler <tomscambler@gmail.com>
2022-08-11 15:17:53 +0100

    added bus id validation

--- bus_board.js
+++ bus_board.js
@@ -1,8 +1,29 @@
-//const fetch = require('node-fetch');
+function getBusStopIdFromUser(myMessageToUser){
+
+    const readline = require('readline-sync');
+
+    console.log(myMessageToUser);
+    
+    let busId = readline.prompt(myMessageToUser);
+    //number = parseInt(number);
+
+    while( !busId.match(/^[0-9]{9}[a-z]/gi) ){
+        console.log("ERROR: That is not a valid bus stop!")
+        busId = readline.prompt(myMessageToUser);
+        //number = parseInt(number);
+    }
+
+    return busId;
+}
+
+
+
+
+let myBusStopId = getBusStopIdFromUser("enter a bus id");
 
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
-fetch("https://api.tfl.gov.uk/StopPoint/910GDENMRKH/ArrivalDepartures?lineIds=london-overground")
+fetch(`https://api.tfl.gov.uk/StopPoint/${myBusStopId}/Arrivals`)
 
 .then(response => response.json())
 .then(body => console.log(body));
