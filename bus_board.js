fc8097e0cb9494d5b16feae1899e90e5454c93b4
Oli Goodall <Oli.Goodall@softwire.com>
2022-08-12 11:27:13 +0100

    Takes Postcodes as input

--- bus_board.js
+++ bus_board.js
@@ -1,7 +1,25 @@
 import readline from "readline-sync";
 
-function isValidBusId(myInput){
-    return myInput.match(/^[0-9]{9}[a-z]$/gi);
+// function isValidBusId(myInput){
+//     return myInput.match(/^[0-9]{9}[a-z]$/gi);
+// }
+
+// function getInputFromUser(myMessageToUser){
+
+//     console.log(myMessageToUser);
+    
+//     let myInput = readline.prompt(myMessageToUser);
+//     myInput = "490004486k";
+
+//     while( !isValidBusId(myInput) ){
+//         console.log("ERROR: That is not valid!")
+//         myInput = readline.prompt(myMessageToUser);
+//     }
+//     return myInput;
+// }
+
+function isValidPostCode(myInput){
+    return myInput.match(/^(([A-Z][0-9]{1,2})|(([A-Z][A-HJ-Y][0-9]{1,2})|(([A-Z][0-9][A-Z])|([A-Z][A-HJ-Y][0-9]?[A-Z]))))\s*[0-9][A-Z]{2}$/gi);
 }
 
 function getInputFromUser(myMessageToUser){
@@ -9,9 +27,8 @@ function getInputFromUser(myMessageToUser){
     console.log(myMessageToUser);
     
     let myInput = readline.prompt(myMessageToUser);
-    myInput = "490004486k";
 
-    while( !isValidBusId(myInput) ){
+    while( !isValidPostCode(myInput) ){
         console.log("ERROR: That is not valid!")
         myInput = readline.prompt(myMessageToUser);
     }
@@ -27,21 +44,27 @@ function secondsToMinutesAndSeconds(numberOfSeconds){
     }
 }
 
-let myBusStopId = getInputFromUser("enter a bus id");
+let myPostCode = getInputFromUser("Enter a postcode");
 
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
-const arrivals = await fetch(`https://api.tfl.gov.uk/StopPoint/${myBusStopId}/Arrivals`)
+const postcodeInfo = await fetch(`http://api.postcodes.io/postcodes/${myPostCode}`)
 
 .then(response => response.json())
-.then(body => body.slice(0,5));
 
-arrivals.sort(function(a, b){return a.timeToStation - b.timeToStation});
 
-for( let i=0; i<5; i++ ){
+console.log(postcodeInfo)
+
+
+//.then(body => body.slice(0,2));
+// arrivals.sort(function(a, b){return a.timeToStation - b.timeToStation});
+
+// for( let i=0; i<5; i++ ){
         
-    let myDestination = arrivals[i].destinationName;
-    let myArrivalTime = arrivals[i].timeToStation;
+//     let myDestination = arrivals[i].destinationName;
+//     let myArrivalTime = arrivals[i].timeToStation;
 
-    console.log(`your next bus to ${myDestination} arrives in ${secondsToMinutesAndSeconds(myArrivalTime)}`);
-}
+//     console.log(`your next bus to ${myDestination} arrives in ${secondsToMinutesAndSeconds(myArrivalTime)}`);
+// }
+
+
