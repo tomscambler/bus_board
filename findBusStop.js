9c919df95176f767526b605c99a8f756645e0ec7
tomscambler <tomscambler@gmail.com>
2022-08-12 11:24:00 +0100

    added bus stop finder

---
+++ findBusStop.js
@@ -0,0 +1,22 @@
+//function findTwoNearestBusStops(myLat, myLon){
+
+    let myLat = 51.5;
+    let myLon = 0.12;
+    let stopTypes = "NaptanPublicBusCoachTram";
+    let radius = 25;
+
+    const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+
+    let arrivals = await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${myLat}&lon=${myLon}&stopTypes=${stopTypes}&radius=${radius}`)
+    .then(response => response.json());
+
+    while (arrivals.stopPoints.length<2){
+        radius+=25;
+        arrivals = await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${myLat}&lon=${myLon}&stopTypes=${stopTypes}&radius=${radius}`)
+        .then(response => response.json());
+    }
+
+    console.log( [arrivals.stopPoints[0].naptanId, arrivals.stopPoints[1].naptanId] );
+//}
+//console.log(findTwoNearestBusStops(51.5, 0.12));
